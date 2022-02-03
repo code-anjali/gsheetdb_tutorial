cbf51a601ec7fae2e29b4357839465ef685b1dbe
code-anjali <98937257+code-anjali@users.noreply.github.com>
2022-02-02 22:44:47 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,11 +18,12 @@ conn = connect(credentials=credentials)
 @st.cache(ttl=600)
 def run_query(query):
     rows = conn.execute(query, headers=1)
-    return rows
+    return {"rows": rows}
+
 
 sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+rows_dict = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # Print results.
-for row in rows:
+for row in rows_dict["rows"]:
     st.write(f"{row.name} has a :{row.pet}:")
