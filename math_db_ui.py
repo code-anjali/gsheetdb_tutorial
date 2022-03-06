86bcb38aaa5e53613f5357c63d18559b826a85bd
Niket Tandon <nikett@allenai.org>
2022-03-06 01:47:35 -0800

    changes to work on streamlit deployment

--- math_db_ui.py
+++ math_db_ui.py
@@ -1,4 +1,5 @@
 import json
+import tempfile
 
 import pandas as pd
 import streamlit as st
@@ -9,12 +10,28 @@ import logging
 logging.basicConfig(format="%(asctime)s - %(message)s", datefmt="%m/%d/%Y %H:%M:%S", level=logging.INFO)
 
 
-def establish_connection(sheet_key, sheet_names_json=".streamlit/sheet_names.json"):
+def fill_secrets_from_streamlit(credentials_empty_fp):
+    credentials_filled_temp_fp = tempfile.NamedTemporaryFile(delete=False, suffix='.json').name
+    with open(credentials_filled_temp_fp, 'w') as credentials_filled_temp:
+        with open(credentials_empty_fp, 'r') as credentials_empty:
+            credentials_empty_entries = json.load(credentials_empty)
+            for key_to_copy in ["private_key", "private_key_id", "client_id"]:
+                credentials_empty_entries[key_to_copy] = st.secrets[key_to_copy]
+            json.dump(credentials_empty_entries, credentials_filled_temp)
+    return credentials_filled_temp_fp
+
+
+def establish_connection(sheet_key,
+                         in_localhost=False,
+                         sheet_names_json_fp="sheet_names.json",
+                         credentials_empty_fp="credentials_empty.json",
+                         local_secret_fp=".streamlit/googlesheetdb-credentials.json"
+                         ):
     if "conn" not in st.session_state or "sheet_url" not in st.session_state or st.session_state.conn.closed:
-        with open(sheet_names_json, 'r') as sheets:
+        with open(sheet_names_json_fp, 'r') as sheets:
             secrets = json.load(sheets)
         st.session_state.conn = connect(":memory:", adapter_kwargs={
-            "gsheetsapi": {"service_account_file": ".streamlit/googlesheetdb-credentials.json"}})
+            "gsheetsapi": {"service_account_file": local_secret_fp if in_localhost else fill_secrets_from_streamlit(credentials_empty_fp)}})
         st.session_state.sheet_url = secrets[sheet_key]
         logging.info(f"Connection to {st.session_state.sheet_url} established? {'no' if st.session_state.conn.closed else 'yes'}")
 
@@ -28,18 +45,14 @@ def search_submissions_query(email_arr_query):
     return rows
 
 
-
-
 def print_results(rows: Cursor, header):
     # ValueError: could not convert string to float: '6,931' => Goggle sheets- Format -> number -> plain text
     logging.info(f"Retrieved: {rows.rowcount if rows else '0'} submissions.")
     st.table(data=pd.DataFrame([row for row in rows], columns=header))
 
 
-
-
 if __name__ == '__main__':
-    establish_connection(sheet_key="math challenge")
+    establish_connection(sheet_key="Math Challenge results.")
     with st.form("form1"):
         st.title("Search")
         student_query = st.text_input("Search by parent email ids")
@@ -52,15 +65,5 @@ if __name__ == '__main__':
             result = search_submissions_query(email_arr_query=student_query.strip().lower().split(','))
             print_results(result, header=header)
 
-        # st.title("Update")
-        # pet_to_update = st.selectbox("Pet:" , ['dog', 'cat', 'mouse', 'bird'])
-        # update_pet = st.text_input("Update the pet name")
-        # user_clicked = st.form_submit_button(label="Update")
-        # if user_clicked:
-        #     logging.info(f"field_to_update (PET); item to update = {pet_to_update} , replace_with_value = {update_pet}")
-        #     result = update_pet_query(pet_to_update.strip().lower(), update_pet=update_pet.strip().lower())
-        #     print_updated_rows(result)
-        #
-        # st.title("Delete")
 
 
